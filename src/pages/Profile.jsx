e59f8918eff24f323ee7d47036d8a060965a61ae
MihirShinde-29 <mihirushinde29@gmail.com>
2024-03-30 16:28:37 +0530

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,33 +1,54 @@
 import { MdEmail } from "react-icons/md";
 import { FaBirthdayCake } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
-import Header from "../components/Header/Header";
 const Profile = () => {
   return (
     <div>
-      <Header />
-      <div className="px-64 bg-gradient-to-r from-[#2eb6b8] via-blue-300  to-[#DAF0F4] w-full h-64 relative">
-        <div className="flex justify-between absolute  top-[100px] ">
-          <div className="shadow bg-white shadow-gray-300 p-8 flex flex-col rounded h-fit">
-            <div
-              className="w-[80px] h-[80px] rounded-full object-cover self-center"
-            ></div>
-            <h1 className="font-bold text-lg cursor-pointer mt-4 self-center">
+      <div className="px-60 py-14 bg-gradient-to-r from-[#2eb6b8] via-blue-300  to-[#DAF0F4] w-full h-64 relative">
+        <div className="flex items-center pb-4">
+          <div className="w-[150px] h-[150px] bg-black rounded-full object-cover self-center"></div>
+          <div className="ml-5">
+            <h1 className="font-extrabold text-[35px] cursor-pointer ml-4 self-center">
               Mihir
             </h1>
-            <div className="mt-4">
+            <h4 className="font-bold text-[23px] cursor-pointer ml-4 self-center">
+              Company Name
+            </h4>
+          </div>
+        </div>
+
+        <div className="flex justify-between absolute mb-8 ">
+          <div className="shadow bg-white shadow-gray-300 p-8 w-96 flex flex-col rounded h-fit">
+            {/* <img
+              className="w-[80px] h-[80px] rounded-full object-cover self-center"
+              src="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg"
+            /> */}
+
+            {/* <h1 className="font-bold text-lg cursor-pointer mt-4 self-center">
+              Angelina
+            </h1> */}
+            <div>
               <div className="flex items-center mt-2">
                 <MdEmail />
-                <p className="ml-2"></p>
+                <p className="ml-2">disha@gmail.com</p>
               </div>
               <div className="flex items-center mt-2">
                 <FaBirthdayCake />
                 <p className="ml-2">17/06/2002</p>
               </div>
+              <div className="stars ml-2 flex mt-8">
+                <b> Rating </b>
+              </div>
+              <div className="stars ml-2 flex items-center mt-2">
+                <div id="star-rate"></div>
+              </div>
             </div>
+            <button className="uppercase justify-center rounded-full border w-[19vw] py-2 hover:bg-light-blue  hover:text-blue bg-blue text-white transition-all duration-100 mt-8">
+              Connect Now
+            </button>
           </div>
-          <div className="mb-8">
-            <div className="ml-8 mt-32 shadow bg-white shadow-gray-300  p-[20px] rounded min-w-[600px]">
+          <div>
+            <div className="ml-8  shadow bg-white shadow-gray-300  p-[20px] rounded min-w-[600px]">
               <div className="flex justify-between border-b-[1px] border-solid border-gray-300 pb-2 mb-2">
                 <h1 className="font-semibold text-lg cursor-pointer self-center ">
                   About
@@ -36,49 +57,30 @@ const Profile = () => {
               </div>
               <div>
                 <p className="font-semibold text-base">Experience : </p>
-                  <>
-                    <p>
-                      <span className="font-semibold">Company</span> :{" "}
-                      1
-                    </p>
-                    <p>
-                      <span className="font-semibold">Role</span> :{" "}
-                      3
-                    </p>
-                    <p>
-                      <span className="font-semibold">Period</span> :{" "}
-                      4
-                    </p>
-                    <p>
-                      <span className="font-semibold">Location</span> :{" "}
-                     5
-                    </p>
-                    <p>
-                      <span className="font-semibold">Industry</span> :{" "}
-                      6
-                    </p>
-                  </>
+                <p>
+                  <span className="font-semibold">Company</span> : Google
+                </p>
+                <p>
+                  <span className="font-semibold">Role</span> : Software
+                  Engineer
+                </p>
+                <p>
+                  <span className="font-semibold">Period</span> : 2020 June -
+                  2021 July
+                </p>
               </div>
               <div className="mt-4">
                 <p className="font-semibold text-base">Education : </p>
-                  <>
-                    <p>
-                      <span className="font-semibold">College</span> :{" "}
-                      
-                    </p>
-                    <p>
-                      <span className="font-semibold">Year</span> :{" "}
-                      
-                    </p>
-                    <p>
-                      <span className="font-semibold">Course</span> :{" "}
-                      
-                    </p>
-                    <p>
-                      <span className="font-semibold">Grade</span> : {" "}
-                      cgpa
-                    </p>
-                  </>
+                <p>
+                  <span className="font-semibold">College</span> : DJ Sanghvi
+                </p>
+                <p>
+                  <span className="font-semibold">Year</span> : 2018-2022
+                </p>
+                <p>
+                  <span className="font-semibold">Course</span> : Computer
+                  Engineering
+                </p>
               </div>
             </div>
             <div className="ml-8 mt-4 shadow bg-white shadow-gray-300  p-[20px] rounded min-w-[600px] max-w-[700px]">
@@ -89,7 +91,48 @@ const Profile = () => {
                 <AiFillEdit size={23} color="#2eb6b8" />
               </div>
               <div>
-                
+                <div className="mt-2 flex items-center border-b-2 border-solid border-gray-200 pb-4">
+                  <img
+                    className="w-[60px] h-[60px] rounded-full object-cover"
+                    src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png"
+                  />
+                  <div className="ml-4">
+                    <h1 className="font-extrabold text-lg cursor-pointer mt-4 self-center">
+                      Tesla
+                    </h1>
+                    <p>
+                      <span className="font-semibold">Description: </span>{" "}
+                      Tesla, Inc. is an American multinational automotive and
+                      clean energy company headquartered in Austin, Texas. Tesla
+                      designs and manufactures electric vehicles (electric cars
+                      and trucks), battery energy storage from home to
+                      grid-scale, solar panels and solar roof tiles, and related
+                      products and services.
+                    </p>
+                    <p className="font-semibold">2020 June - Present</p>
+                  </div>
+                </div>
+                <div className="mt-2 flex items-center border-b-2 border-solid border-gray-200 pb-4">
+                  <img
+                    className="w-[60px] h-[60px] rounded-full object-cover"
+                    src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png"
+                  />
+                  <div className="ml-4">
+                    <h1 className="font-bold text-lg cursor-pointer mt-4 self-center">
+                      Tesla
+                    </h1>
+                    <p>
+                      <span className="font-semibold">Description: </span>{" "}
+                      Tesla, Inc. is an American multinational automotive and
+                      clean energy company headquartered in Austin, Texas. Tesla
+                      designs and manufactures electric vehicles (electric cars
+                      and trucks), battery energy storage from home to
+                      grid-scale, solar panels and solar roof tiles, and related
+                      products and services.
+                    </p>
+                    <p className="font-semibold">2020 June - Present</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
