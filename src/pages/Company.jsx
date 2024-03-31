4b208648496be1baaf69409216432a606e5c9b12
MihirShinde-29 <mihirushinde29@gmail.com>
2024-03-31 07:06:47 +0530

    company page

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -5,6 +5,7 @@ import companyVideo from "../assets/company.mp4";
 import company1 from "../assets/company1.jpg";
 import company2 from "../assets/company2.jpeg";
 import JobCard from "../components/JobCard";
+import { Link } from "react-router-dom";
 
 const Company = () => {
   return (
@@ -17,10 +18,20 @@ const Company = () => {
             <div className="text-4xl font-semibold">Apple</div>
           </div>
           <Carousel
-            sx={{ width: "100%", height: "50%", margin: "30px 0px" }}
+            sx={{ width: "100%", height: "55vh", margin: "30px 0px" }}
             animation="slide"
             interval={20000}
           >
+            <iframe
+              width="100%"
+              height="440"
+              src="https://www.youtube.com/embed/zDAYZU4A3w0?autoplay=1?si=LgVIOyByaM_pjDXs&amp;start=47"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
             <video autoPlay loop muted className="absolute -z-10 w-full h-auto">
               <source src={companyVideo} type="video/mp4" />
             </video>
@@ -40,12 +51,51 @@ const Company = () => {
             societal impact around the world.
           </div>
           <div className="text-xl font-semibold my-2">Jobs Available</div>
-          <div className="flex gap-4 py-2 w-full overflow-x-auto scrollbar-hide">
+          <div className="flex gap-4 py-4 w-full overflow-x-auto scrollbar-hide">
             <JobCard />
             <JobCard />
             <JobCard />
             <JobCard />
             <JobCard />
+          </div>
+          <div className="text-xl font-semibold my-2">Employee Reviews</div>
+          <div className="flex gap-4 items-center px-4 py-2 rounded-xl my-4">
+            <img src={logo} alt="" className="h-20 w-20 object-cover" />
+            <div className="">
+              <div className="font-semibold text-lg">
+                Apple fosters a culture of innovation and excellence, where
+                every day presents new challenges and opportunities for personal
+                and professional growth, making it an exhilarating place to
+                work.
+              </div>
+              <Link to="profile/123" className="text-sm text-gray-400">
+                -Dishant Zaveri
+              </Link>
+            </div>
+          </div>
+          <div className="flex gap-4 items-center px-4 py-2 rounded-xl my-4">
+            <img src={logo} alt="" className="h-20 w-20 object-cover" />
+            <div className="">
+              <div className="font-semibold text-lg">
+                Working at Apple is an inspiring journey fueled by innovation,
+                collaboration, and a relentless dedication to delivering
+                exceptional products that positively impact the world.
+              </div>
+              <Link to="profile/123" className="text-sm text-gray-400">
+                -Dishant Zaveri
+              </Link>
+            </div>
+          </div>
+          <div className="flex gap-4 items-center px-4 py-2 rounded-xl my-4">
+            <img src={logo} alt="" className="h-20 w-20 object-cover" />
+            <div className="">
+              <div className="font-semibold text-lg">
+                Best working environment and amazing food .
+              </div>
+              <Link to="profile/123" className="text-sm text-gray-400">
+                -Dishant Zaveri
+              </Link>
+            </div>
           </div>
         </div>
         <div className="px-8 py-6 w-full col-span-4">
@@ -61,6 +111,15 @@ const Company = () => {
           <div className="flex gap-4 items-center border-2 hover:shadow-lg px-4 py-2 rounded-xl my-4">
             <img src={logo} alt="" className="h-8 w-8 object-cover" />
             <div className="font-semibold">Dishant Zaveri</div>
+          </div>
+          <div className="text-lg font-semibold">Similar Companies</div>
+          <div className="flex gap-4 items-center border-2 hover:shadow-lg px-4 py-2 rounded-xl my-4">
+            <img src={logo} alt="" className="h-8 w-8 object-cover" />
+            <div className="font-semibold">Google</div>
+          </div>
+          <div className="flex gap-4 items-center border-2 hover:shadow-lg px-4 py-2 rounded-xl my-4">
+            <img src={logo} alt="" className="h-8 w-8 object-cover" />
+            <div className="font-semibold">Meta</div>
           </div>
         </div>
       </div>
