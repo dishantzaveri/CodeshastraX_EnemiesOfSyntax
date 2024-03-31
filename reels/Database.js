ca13488c54cc2b20a034def858a626a4683375e6
dishantzaveri <zaveridishant@gmail.com>
2024-03-31 10:16:17 +0530

    integrations and redux

---
+++ reels/Database.js
@@ -0,0 +1,36 @@
+// Videos Data
+
+export const videoData = [
+  {
+    video: require('./videos/video1.mp4'),
+    postProfile: require('./images/post1.jpg'),
+    title: 'Dishant Zaveri',
+    description: 'Feel the buity of nature',
+    likes: '245k',
+    isLike: false,
+  },
+  {
+    video: require('./videos/video2.mp4'),
+    postProfile: require('./images/post2.jpg'),
+    title: 'The_Groot',
+    description: "It's a tea time",
+    likes: '656k',
+    isLike: false,
+  },
+  {
+    video: require('./videos/video3.mp4'),
+    postProfile: require('./images/post3.jpg'),
+    title: 'loverland',
+    description: 'Feel the buity of nature',
+    likes: '243k',
+    isLike: false,
+  },
+  {
+    video: require('./videos/video4.mp4'),
+    postProfile: require('./images/post4.jpg'),
+    title: 'mr. bean',
+    description: 'How cute it is !!',
+    likes: '876k',
+    isLike: false,
+  },
+];
